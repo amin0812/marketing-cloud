ec379667740b2c575edfe5c85196999877cb1d17
Mohamed amin ELAOUD <49599841+amin0812@users.noreply.github.com>
2022-06-22 16:11:05 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,13 @@
-<script runat="server">
-  Platform.Load('Core', '1.1')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Running code from Github private repositories</title>
+</head>
+ <body>
+   <!-- Getting the response from SSJS and treat as a content in the HTML -->
+  %%=TreatAsContent(@runCode)=%%
 
-  // Global Variables
-  // Change these variables according to your own settings
-  var userDetails = {
-           username : marketingdeveloper",
-           token    : "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",
-           repoName : "marketing-cloud",
-           fileName : "test.js"
-        }
-  // Do not change anything below this section
-  // get repo content
-  function getPrivateRepoContent(obj){
-
-      //Building the request URL = https://api.github.com/repos/marketingdeveloper/sfmc/contents/dev-1.html
-      var resource = 'https://api.github.com/repos/'+ obj.username + '/' + obj.repoName + '/contents/' + obj.fileName
-
-      var req = new Script.Util.HttpRequest(resource);
-          req.emptyContentHandling = 0;
-          req.retries = 2;
-          req.continueOnError = true;
-          req.contentType = "application/json; charset=utf-8"
-          req.setHeader("Authorization","token " + obj.token);
-          req.setHeader("User-Agent", obj.username + '/' + obj.repoName);
-          //  This header is very important! It allows to get the file content raw version. With that we can treat the the file as content like a charm :)
-          req.setHeader("Accept", "application/vnd.github.v3.raw");
-          req.setHeader("Cache-Control", "");
-          req.method = "GET";
-
-      var resp = req.send();
-
-      // Assigning the response content to a AMPScript variable
-      Platform.Variable.SetValue("@runCode", resp.content)
-  }
-
-  //Run the function passing the user settings as parameter
-  getPrivateRepoContent(userDetails)
-</script>
+   </body>
+</html>
