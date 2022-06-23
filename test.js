51403ef0777a66f8cb22c20e4c56d12a06863c96
Mohamed amin ELAOUD <49599841+amin0812@users.noreply.github.com>
2022-06-23 12:53:06 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Running code from Github private repositories</title> 
-    <p> Hedha howa </p>
-</head>
- <body>
-   <!-- Getting the response from SSJS and treat as a content in the HTML -->
-  %%=TreatAsContent(@runCode)=%%
-
-   </body>
-</html>
+123
